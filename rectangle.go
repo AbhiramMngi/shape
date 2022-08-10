082d0efbc674700d54bb874f3978223e001a9acb
Abhiram Reddy <abhiram@Abhirams-MacBook-Air.local>
2022-08-10 23:16:11 +0530

    Create Rectangle test cases

--- rectangle.go
+++ rectangle.go
@@ -5,13 +5,25 @@ type Rectangle struct {
 }
 
 func NewRectangle(length, width float64) Rectangle {
+	if length <= 0 || width <= 0 {
+		panic("length or width should be positive")
+	}
 	return Rectangle{length, width}
 }
 
-func (rectangle Rectangle) FindPerimeter() float64 {
-	return 0.0
+func (rectangle Rectangle) FindArea() float64 {
+	if rectangle.length == 1.0 && rectangle.width == 1.0 {
+		return 1.0
+	}
+	if rectangle.length == 1 {
+		return rectangle.width
+	}
+	if rectangle.width == 1 {
+		return rectangle.length
+	}
+	return rectangle.length * rectangle.width
 }
 
-func (rectangle Rectangle) FindArea() float64 {
+func (rectangle Rectangle) FindPerimeter() float64 {
 	return 0.0
 }
---
+++ rectangle_test.go
@@ -0,0 +1,40 @@
+package Square
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewRectangle(t *testing.T) {
+	t.Run("should check if the rectangle object is created", func(t *testing.T) {
+		assert.IsType(t, Rectangle{}, NewRectangle(1.0, 2.0))
+	})
+	t.Run("should check for panic when length or width is not positive", func(t *testing.T) {
+		assert.Panics(t, func() {
+			NewRectangle(-20.0, 32.4)
+		})
+	})
+	t.Run("should check for no panic when length and width are positive", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			NewRectangle(2.0, 3.0)
+		})
+	})
+}
+
+func TestFindRectangleArea(t *testing.T) {
+	t.Run("should check the area of the rectangle when the length and width are 1", func(t *testing.T) {
+		assert.Equal(t, 1.0, NewRectangle(1.0, 1.0).FindArea())
+	})
+	t.Run("should check the area of the rectangle when the length is 1 and width is not equal to 1", func(t *testing.T) {
+		assert.Equal(t, 5.0, NewRectangle(1.0, 5.0).FindArea())
+	})
+	t.Run("should check the area of the rectangle when the length is not equal to 1 and width is 1", func(t *testing.T) {
+		assert.Equal(t, 5.0, NewRectangle(5.0, 1.0).FindArea())
+	})
+	t.Run("should check the area of the rectangle when the length is not equal to 1 and width is not equal to 1", func(t *testing.T) {
+		assert.Equal(t, 27.0, NewRectangle(9.0, 3.0).FindArea())
+	})
+	t.Run("should check the area of the rectangle when the length and width are swapped", func(t *testing.T) {
+		assert.Equal(t, NewRectangle(3.0, 9.0).FindArea(), NewRectangle(9.0, 3.0).FindArea())
+	})
+}
